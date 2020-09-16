c7806f9cc54649b0cd4f198aad0c237ec64017ee
giovanib02 <giovanib07@gmail.com>
2020-09-16 00:52:27 -0600

    First commit

---
+++ toolboxAPI/routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+const { json } = require('express');
+const { route } = require('../app');
+var router = express.Router();
+
+router.use('/text',(req,res,next)=>{
+  if(!req.body.message){
+    res.send("Message not found!");
+  }
+  next();
+})
+
+router.post('/text',  (req,res,next) =>{
+  //let jsonRequest = JSON.parse(req.body);
+  const message = req.body.message;
+  res.send(message);
+});
+
+router.use((req,res,next) =>{
+  res.send("Welcome to Toolbox API");
+});
+
+module.exports = router;
