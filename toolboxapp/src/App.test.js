94e127b68f5067742f5e8c101aa9eccf50bf4c34
Carlos Barillas <giovanib07@gmail.com>
2020-09-19 15:34:41 -0600

    adding unit testing to react app with jest

--- toolboxapp/src/App.test.js
+++ toolboxapp/src/App.test.js
@@ -1,9 +1,59 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, screen} from '@testing-library/react';
 import App from './App';
+import SendMessage from './components/SendMessage';
+import store from './store';
+import { Provider } from "react-redux"
+import Adapter from 'enzyme-adapter-react-16';
+import {configure, mount} from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({adapter: new Adapter()});
+
+test('Checking that we are using boostrap', () => {
+   const wrapper = mount(<Provider store={store}><App/></Provider>);
+   const form = wrapper.find('.boostrap').exists();
+   expect(form).toBeTruthy();
 });
+
+test('Checking that the form exits', () => {
+  const handleSubmit = jest.fn();
+   const wrapper = mount(<Provider store={store}><SendMessage form="test" handleSubmit={handleSubmit}   /></Provider>);
+   const form = wrapper.find('.mainForm').exists();
+   expect(form).toBeTruthy();
+});
+
+test('Checking that the textarea to enter a text exists', () => {
+  const handleSubmit = jest.fn();
+   const wrapper = mount(<Provider store={store}><SendMessage form="test" handleSubmit={handleSubmit}   /></Provider>);
+   const form = wrapper.find('.inputMessage').exists();
+   expect(form).toBeTruthy();
+});
+
+
+test('Checking that the textarea to receive the response exits', () => {
+  const handleSubmit = jest.fn();
+   const wrapper = mount(<Provider store={store}><SendMessage form="test" handleSubmit={handleSubmit}   /></Provider>);
+   const form = wrapper.find('.responseMessage').exists();
+   expect(form).toBeTruthy();
+});
+
+
+
+test('Testing submit the form to the API', () => {
+     const handleSubmit = jest.fn();
+      const wrapper = mount(<Provider store={store}><SendMessage form="test" handleSubmit={handleSubmit}   /></Provider>);
+      wrapper.find('.mainForm').simulate('submit', {
+        preventDefault: () => {
+        },
+        target: 
+          {
+            userMessage: {
+              value:"message"
+            },
+          }
+        ,
+      });
+      expect(handleSubmit).toBeCalledTimes(1);
+  });
+
+
--- toolboxapp/src/components/SendMessage.js
+++ toolboxapp/src/components/SendMessage.js
@@ -4,27 +4,30 @@ import { connect } from 'react-redux'
 import {sendMessageToAPI} from '../actions'
 
 class SendMessage extends Component {
+
+    handleSubmit = event => {
+        event.preventDefault();
+        let input = event.target.userMessage.value;
+        input !== "" ? this.props.sendMessageToAPI(input): event.target.warningMessage = "test";
+        event.target.userMessage.value = "";
+    };
+
     render() {
         return (
             <div> 
                 <Header></Header>
                 <div className="col-md-4 offset-md-4">
-                <form onSubmit={(event)=>{
-                        event.preventDefault();
-                        let input = event.target.userMessage.value;
-                        this.props.sendMessageToAPI(input);
-                        event.target.userMessage.value = "";
-                    }}>
+                <form className="mainForm" onSubmit={this.props.handleSubmit || this.handleSubmit}>
                     <div className="form-group">
                         <label>Message</label>
-                        <textarea type="text" className="form-control rounded-0" id="txtMessage" placeholder="Enter a Message" rows="3" name="userMessage"></textarea>
+                        <textarea type="text" className="form-control rounded-0 inputMessage" id="txtMessage" data-testid="txtMessage" placeholder="Enter a Message" rows="3" name="userMessage"></textarea>
                         <small  className="form-text text-muted">Write your message here!.</small>
                     </div>
                     <div className="form-group">
                         <label>ToolBoxAPI Answer</label>
-                <textarea  disabled type="text" className="form-control rounded-0" id="txtAnswer" placeholder="You'll see the answer from the API here" rows="3" value={this.props.message}></textarea>
+                <textarea  disabled type="text" className="form-control rounded-0 responseMessage" id="txtAnswer" placeholder="You'll see the answer from the API here" rows="3" value={this.props.message}></textarea>
                     </div>
-                    <button type="submit" className="btn btn-primary">Send Message</button>
+                    <button type="submit" className="btn btn-primary" >Send Message </button>
                 </form>
                 </div>
             </div>
